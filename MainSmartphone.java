b2f2afccda2bb618a8208c6d11dc5955b728424d
Olaxism <arq.medrano@hotmail.com>
2025-11-10 10:29:59 +0100

    Update MainSmartphone.java
    Codigo mas limpio al implementar las interfaces a la clase Smartphone

--- MainSmartphone.java
+++ MainSmartphone.java
@@ -1,31 +1,19 @@
-package sprint1;
+package sprint_1.tasca_1.nivel_2;
 
 public class MainSmartphone {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		Smartphone s1 = new Smartphone ("Samsung", "Galaxy A14", "629-47-37-27");
-		Smartphone s2 = new Smartphone ("Motorola", "Moto G84", "672-57-67-77");
-	    //Telefono t1 = new Telefono ("SonyEricsson", "W910i", "673-21-32-43");
-		//Porque no puedo hacer un telefono que no sea Smart?
-		//Pude hacerlo en el proceso de hacer el ejercicio si la clase Telefono no era abstracta.
-		
-	    Camara c1 = new Camara();
-	    Reloj r1 = new Reloj();
-	    
-	    Camara c2 = new Camara();
-	    Reloj r2 = new Reloj();
-		
-	    s1.añadirCamara(c1);
-	    s1.añadirReloj(r1);
-	    
-	    s2.añadirCamara(c2);
-	    s2.añadirReloj(r2);
-	    
-	    s1.llamar(s2.getNumero()); // El Smartphone 1 esta llamando al Smartphone 2
-	    s1.alarma();
-	    s1.fotografiar();
-	}
+        Smartphone s1 = new Smartphone ("Samsung", "Galaxy A14", "629-47-37-27");
+        Smartphone s2 = new Smartphone ("Motorola", "Moto G84", "672-57-67-77");
+        Smartphone t1 = new Smartphone("SonyEricsson", "W910i", "673-21-32-43");
+
+
+        s1.llamar(s2.getNumero()); // El Smartphone 1 esta llamando al Smartphone 2
+        s1.alarma();
+        s1.fotografiar();
+        t1.alarma();
+    }
 
 }
