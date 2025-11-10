5f54922e6fd0268e3ac11706b96d96900521709c
Olaxism <arq.medrano@hotmail.com>
2025-11-10 11:36:50 +0100

    Update MainIntrumento.java
    Comentarios eliminados e incluido en el system out

--- MainIntrumento.java
+++ MainIntrumento.java
@@ -1,21 +1,23 @@
 package sprint_1.tasca_1.nivel_1.ejercicio_1;
 
+import sprint_1.tasca_1.nivel_1.ejercicio_1.classes.Instrumento;
+import sprint_1.tasca_1.nivel_1.ejercicio_1.classes.InstrumentoCuerda;
+import sprint_1.tasca_1.nivel_1.ejercicio_1.classes.InstrumentoPercusion;
+import sprint_1.tasca_1.nivel_1.ejercicio_1.classes.InstrumentoViento;
+
 public class MainInstrumento {
 
-   public static void main(String[] args) {
-    System.out.println("Comencemos a tocar");
+    public static void main(String[] args) {
+        System.out.println("Comencemos a tocar");
 
-    InstrumentoPercusion i1 = new InstrumentoPercusion ("Xilofono", 150.75);
-    InstrumentoViento i2 = new InstrumentoViento ("Fagot", 120.90);
-    InstrumentoCuerda i3 = new InstrumentoCuerda ("Bajo", 284.99);
+        InstrumentoPercusion i1 = new InstrumentoPercusion("Xilofono", 150.75);
+        InstrumentoViento i2 = new InstrumentoViento("Fagot", 120.90);
+        InstrumentoCuerda i3 = new InstrumentoCuerda("Bajo", 284.99);
 
-    i1.tocar();
-    i3.tocar();
-    i2.tocar();
+        i1.tocar();
+        i3.tocar();
+        i2.tocar();
 
-    System.out.println(Instrumento.marca); //Ejecucion de bloque estatico a traves de un atributo.
-
-   }
+        System.out.println("Comprobacion del metodo estatico desde un atributo de la clase" + Instrumento.marca);
+    }
 }
-
-
