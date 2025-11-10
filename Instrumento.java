b4c21ca79cb3a4d2d94ec989d87be7e1a520c773
Olaxism <arq.medrano@hotmail.com>
2025-11-10 10:25:41 +0100

    Update Instrumento.java
    Clase abstracta y metodos abstractos. Con bloque estatico para acceder a el a traves de un atributo.

--- Instrumento.java
+++ Instrumento.java
@@ -1,38 +1,33 @@
-package sprint1;
+package sprint_1.tasca_1.nivel_1.ejercicio_1;
 
-public class Instrumento {
+public abstract class Instrumento {
 
-	private String nombre;
-	private double precio;
-	private static String tocando;
-	
-	static {
-	System.out.println("Bloque estatico ejecutado");
-	}
-	
-	public Instrumento (String nombre, double precio, String tocando) {
-	this.nombre = nombre;
-	this.precio = precio;
-	Instrumento.tocando = tocando;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
-	public double getPrecio() {
-		return precio;
-	}
-	
-	public String tocar() {
-		System.out.print(tocando);
-		return tocando;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "\nInstrumento {Nombre= " + getNombre() +
-		", Precio= " + getPrecio() + " euros.}";}
-}
+    private String nombre;
+    private double precio;
+    public static String marca;
 
+    static {
+        System.out.println("Bloque estatico ejecutado");
+     marca = "JBL";
+    }
+
+    public Instrumento (String nombre,double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public abstract void tocar();
+
+    @Override
+    public String toString() {
+        return "\nInstrumento {Nombre= " + getNombre() +
+                ", Precio= " + getPrecio() + " euros.}";}
+    }
