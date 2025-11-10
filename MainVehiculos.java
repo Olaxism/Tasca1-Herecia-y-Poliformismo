ef19622e8a7f9790a2d9e1d930de49125198474d
Olaxism <arq.medrano@hotmail.com>
2025-11-10 11:39:56 +0100

    Update MainVehiculos.java
    Se refleja el nuevo uso del constructor de la clase Vehiculo

--- MainVehiculos.java
+++ MainVehiculos.java
@@ -1,5 +1,7 @@
 package sprint_1.tasca_1.nivel_1.ejercicio_2;
 
+import sprint_1.tasca_1.nivel_1.ejercicio_2.classes.Vehiculo;
+
 public class MainVehiculos {
 
     public static void main(String[] args) {
@@ -8,14 +10,12 @@ public class MainVehiculos {
         System.out.println("");
 
 
-        Vehiculo v1 = new Vehiculo ("Camry");
-        Vehiculo v2 = new Vehiculo ("Corolla");
+        Vehiculo v1 = new Vehiculo ();
+        Vehiculo v2 = new Vehiculo ();
 
 
         v1.acelerar();
         Vehiculo.frenar();
         v2.acelerar();
     }
-
 }
-
