b4b5bfa8d177b72a4004ba2ee02e3aab42dda5f2
Olaxism <arq.medrano@hotmail.com>
2025-11-10 10:36:51 +0100

    Update Smartphone.java
    Codigo mas limpio implementando las interfaces

--- Smartphone.java
+++ Smartphone.java
@@ -1,51 +1,16 @@
-package sprint1;
+package sprint_1.tasca_1.nivel_2;
 
-import java.util.ArrayList;
 
-public class Smartphone extends Telefono {
-	
-	private ArrayList<Camara> camaras;
-	private ArrayList<Reloj> relojes;
+public class Smartphone extends Telefono implements Camara, Reloj {
 
-	public Smartphone (String marca, String modelo, String numero) {
-		super (marca, modelo, numero);
-		this.camaras = new ArrayList<>();
-		this.relojes = new ArrayList<>();
-	}
-	
-	public void añadirCamara(Camara camara) {
-		camaras.add(camara);
-	}
+    public Smartphone (String marca, String modelo, String numero) {
+        super (marca, modelo, numero);
+    }
 
-	public void añadirReloj(Reloj reloj) {
-		relojes.add(reloj);
-	}
-	
-	public void fotografiar() {
-		if (!camaras.isEmpty()) {
-			for(Camara camara : camaras) {
-				camara.fotografiar();
-			}
-		}else {
-			System.out.println("Ups! Al parecer no tiene camaras este teléfono");
-		}
-	}
-	
-	public void alarma() {
-		if (!relojes.isEmpty()) {
-			for (Reloj reloj : relojes) {
-				reloj.alarma();
-			}
-		}else {
-			System.out.println("Al parecer el reloj de este telefono no tiene alarma");
-		}
-	}
-
-	@Override
-	public String toString() {
-		return "Smartphone [camaras= " + camaras + ", relojes= " + relojes + ", getMarca= " + getMarca()
-				+ ", getModelo= " + getModelo() + ", getNumero= " + getNumero() + "]";
-	}
-	
-	
+    @Override
+    public String toString() {
+        return "Smartphone [Marca= " + getMarca()
+                + ", Modelo= " + getModelo() + ", Numero= " + getNumero() + "]";
+    }
+    
 }
