de9990e23dd645b749125d954a814d6bf6120789
Olaxism <arq.medrano@hotmail.com>
2025-11-10 10:38:15 +0100

    Update Telefono.java
    Puesto en carpeta

--- Telefono.java
+++ Telefono.java
@@ -1,38 +1,39 @@
-package sprint1;
+package sprint_1.tasca_1.nivel_2;
 
 public abstract class Telefono {
 
-	private String marca;
-	private String modelo;
-	private String numero;
-	
-	public Telefono (String marca, String modelo, String numero) {
-		this.marca = marca;
-		this.modelo = modelo;
-		this.numero = numero;
-	}
-	
-	public String getMarca() {
-		return marca;
-	}
-	
-	public String getModelo() {
-		return modelo;
-	}
-	
-	public String getNumero() {
-		return numero;
-	}
-	
-	public String llamar(String numElegido) {
-		System.out.println("Llamando al número: " + numElegido);
-		return numElegido;
-	}
+    private String marca;
+    private String modelo;
+    private String numero;
 
-	@Override
-	public String toString() {
-		return "Telefono [marca= " + getMarca() + ", modelo= " + getModelo() + ", numero= " + getNumero() + "]";
-	}
+    public Telefono (String marca, String modelo, String numero) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numero = numero;
+    }
 
-  }
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String llamar(String numElegido) {
+        System.out.println("Llamando al número: " + numElegido);
+        return numElegido;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono [marca= " + getMarca() + ", modelo= " + getModelo() + ", numero= " + getNumero() + "]";
+    }
+
+}
+
 
