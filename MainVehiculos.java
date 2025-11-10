53d9f612361002b77e36f7d967fe791fb4f63f72
Olaxism <arq.medrano@hotmail.com>
2025-11-10 10:34:22 +0100

    Update MainVehiculos.java
    Codigo mas limpio. Sin dejar metodos sin usar.

--- MainVehiculos.java
+++ MainVehiculos.java
@@ -1,25 +1,21 @@
-package sprint1;
+package sprint_1.tasca_1.nivel_1.ejercicio_2;
 
 public class MainVehiculos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Vamos a probar Vehiculos");
-		
-		
-		Vehiculo v1 = new Vehiculo ("Camry");
-		Vehiculo v2 = new Vehiculo ("Corolla");
-		Vehiculo v3 = new Vehiculo ("Supra");
-		Vehiculo v4 = new Vehiculo ("Yaris");
-		
-		v1.acelerar();
-		Vehiculo.frenar();
-		v2.acelerar();
-		v3.acelerar();
-		v4.acelerar();
+    public static void main(String[] args) {
 
-        System.out.println(Vehiculo.getMarca());
-	}
+        System.out.println("\nVamos a probar Vehiculos");
+        System.out.println("");
+
+
+        Vehiculo v1 = new Vehiculo ("Camry");
+        Vehiculo v2 = new Vehiculo ("Corolla");
+
+
+        v1.acelerar();
+        Vehiculo.frenar();
+        v2.acelerar();
+    }
 
 }
+
